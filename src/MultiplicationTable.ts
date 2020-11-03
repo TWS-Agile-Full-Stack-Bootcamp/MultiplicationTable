63d9638fef180e4dc9e77b117bae18814595ce4d
Chen Xu <xuchen@thoughtworks.com>
2020-11-03 16:23:38 +0800

    feat: start should greater than end

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,5 +1,10 @@
 export class MultiplicationTable {
-  render(start: number, end: number): string{
+  render(start: number, end: number): string {
+
+    if (start > end) {
+      return 'nothing'
+    }
+
     return '1*1=1'
   }
 }
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -1,7 +1,11 @@
-import {MultiplicationTable} from '../src/MultiplicationTable'
+import { MultiplicationTable } from '../src/MultiplicationTable'
 
 describe('Multiplication Table', () => {
   it('should render multiplication table of (1, 1)', () => {
     expect(new MultiplicationTable().render(1, 1)).toBe('1*1=1')
+  })
+
+  it('should render noting when start greater than end', () => {
+    expect(new MultiplicationTable().render(1, 0)).toBe('nothing')
   })
 })
