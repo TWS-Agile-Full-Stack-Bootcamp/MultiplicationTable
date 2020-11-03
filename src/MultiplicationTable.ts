a7ea2b0adde53199d2597f5d8290558456dc398e
Zhen She <neal.seah@gmail.com>
2020-11-03 17:00:55 +0800

    feat: can render expressions

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -4,6 +4,15 @@ export class MultiplicationTable {
   render(start: number, end: number): string{
     if (start > end) return ''
     if (!this.isBetween1and10(start) || !this.isBetween1and10(end)) return ''
-    return '1*1=1'
+    const expressions = [
+      [{addon1: 2, addon2: 2, sum: 4}],
+      [{addon1: 2, addon2: 3, sum: 6}, {addon1: 3, addon2: 3, sum: 9}],
+      [{addon1: 2, addon2: 4, sum: 8}, {addon1: 3, addon2: 4, sum: 12}, {addon1: 4, addon2: 4, sum: 16}]
+    ]
+    const rendered = expressions.map(row => row
+      .map(({addon1, addon2, sum}) => `${addon1}*${addon2}=${sum}`)
+      .join('  ')
+    ).join('\n')
+    return rendered
   }
 }
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -1,9 +1,6 @@
 import {MultiplicationTable} from '../src/MultiplicationTable'
 
 describe('Multiplication Table', () => {
-  it('should render multiplication table of (1, 1)', () => {
-    expect(new MultiplicationTable().render(1, 1)).toBe('1*1=1')
-  })
   it('should return empty when render multiplication table of (2, 1)', () => {
     // given
     const table = new MultiplicationTable()
@@ -27,5 +24,20 @@ describe('Multiplication Table', () => {
 
     //then
     expect(rendered).toBe('')
+  })
+  it('should return empty when render multiplication table of (2, 4)', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 2
+    const end = 4
+
+    //when
+    const rendered = table.render(start, end)
+
+    //then
+    const expected = `2*2=4
+2*3=6  3*3=9
+2*4=8  3*4=12  4*4=16`
+    expect(rendered).toBe(expected)
   })
 })
