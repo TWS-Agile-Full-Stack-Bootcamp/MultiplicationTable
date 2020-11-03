e9f2cdf5100251427effeb768f1746b7dac5a93c
Zhen She <neal.seah@gmail.com>
2020-11-03 16:45:05 +0800

    refactor: follow GWT test pattern

--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -4,10 +4,28 @@ describe('Multiplication Table', () => {
   it('should render multiplication table of (1, 1)', () => {
     expect(new MultiplicationTable().render(1, 1)).toBe('1*1=1')
   })
-  it('should not render multiplication table of (2, 1)', () => {
-    expect(new MultiplicationTable().render(2, 1)).toBe('')
+  it('should return empty when render multiplication table of (2, 1)', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 2
+    const end = 1
+
+    //when
+    const rendered = table.render(start, end)
+
+    //then
+    expect(rendered).toBe('')
   })
-  it('should not render multiplication table of (1, 11)', () => {
-    expect(new MultiplicationTable().render(1, 11)).toBe('')
+  it('should return empty when render multiplication table of (1, 11)', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 1
+    const end = 11
+
+    //when
+    const rendered = table.render(start, end)
+
+    //then
+    expect(rendered).toBe('')
   })
 })
