a43c550ce77a1f874039d2f22e29b7ab73cee2cb
Zhen She <neal.seah@gmail.com>
2020-11-03 16:31:00 +0800

    feat: return nothing when start is greater than end

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,5 +1,6 @@
 export class MultiplicationTable {
   render(start: number, end: number): string{
+    if (start > end) return ''
     return '1*1=1'
   }
 }
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -4,4 +4,7 @@ describe('Multiplication Table', () => {
   it('should render multiplication table of (1, 1)', () => {
     expect(new MultiplicationTable().render(1, 1)).toBe('1*1=1')
   })
+  it('should not render multiplication table of (2, 1)', () => {
+    expect(new MultiplicationTable().render(2, 1)).toBe('')
+  })
 })
