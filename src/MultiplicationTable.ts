3b2ab384b0e97b046af8a4fe47611eb3d9207f88
Zhen She <neal.seah@gmail.com>
2020-11-03 17:57:17 +0800

    feat: can align expression when they their diff width

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,3 +1,9 @@
+interface Expression {
+  addon1: number
+  addon2: number
+  sum: number
+}
+
 export class MultiplicationTable {
   isBetween1and10 = (target: number) => (target >= 1 && target <= 10)
 
@@ -15,14 +21,23 @@ export class MultiplicationTable {
     return expressions
   }
 
+  renderExpressions = (start: number, end: number, expressions: Expression[][]) => {
+    const width = Array(end - start + 1).fill(0)
+    return expressions.reverse().map((row: Expression[]) => row
+      .map(({addon1, addon2, sum}: Expression) => {
+        const expression = `${addon1}*${addon2}=${sum}`
+        const col = addon1 - start
+        width[col] = Math.max(width[col], expression.length)
+        const spacing = ' '.repeat(width[col] - expression.length)
+        return expression + spacing
+      }).join('  ').trim()
+    ).reverse().join('\n')
+  }
+
   render(start: number, end: number): string{
     if (start > end) return ''
     if (!this.isBetween1and10(start) || !this.isBetween1and10(end)) return ''
     const expressions = this.buildExpressions(start, end)
-    const rendered = expressions.map(row => row
-      .map(({addon1, addon2, sum}) => `${addon1}*${addon2}=${sum}`)
-      .join('  ')
-    ).join('\n')
-    return rendered
+    return this.renderExpressions(start, end, expressions)
   }
 }
