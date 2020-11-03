a16069da28f6d50f53ff94ef18c478ada885389c
Wu Xueheng <xhwu@thoughtworks.com>
2020-11-03 16:44:31 +0800

    feat: implement isStartNumberLessThanOrEqualsToEndNumber

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -2,4 +2,9 @@ export class MultiplicationTable {
   render(start: number, end: number): string{
     return '1*1=1'
   }
+
+  isStartNumberLessThanOrEqualsToEndNumber(start: number, end: number): boolean{
+    return start <= end
+  }
+
 }
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -4,4 +4,12 @@ describe('Multiplication Table', () => {
   it('should render multiplication table of (1, 1)', () => {
     expect(new MultiplicationTable().render(1, 1)).toBe('1*1=1')
   })
+
+  it('should return true when check given start number less than end number', () => {
+    expect(new MultiplicationTable().isStartNumberLessThanOrEqualsToEndNumber(1, 2)).toBeTruthy();
+  })
+
+  it('should return true when check given start number equals to end number', () => {
+    expect(new MultiplicationTable().isStartNumberLessThanOrEqualsToEndNumber(1, 1)).toBeTruthy();
+  })
 })
