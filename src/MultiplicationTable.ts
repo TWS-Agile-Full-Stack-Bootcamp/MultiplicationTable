c0dfed80f679c9e6983fbdd4095668b58aeeb8d5
Zhen She <neal.seah@gmail.com>
2020-11-03 18:26:45 +0800

    refactor: extract methods

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -5,8 +5,6 @@ interface Expression {
 }
 
 export class MultiplicationTable {
-  isBetween1and10 = (target: number) => (target >= 1 && target <= 10)
-
   buildExpressions = (start: number, end: number) => {
     const expressions = []
     for (let row = 0; row <= (end - start); row++) {
@@ -34,9 +32,15 @@ export class MultiplicationTable {
     ).reverse().join('\n')
   }
 
+  isValidRange = (start: number, end: number) => (start <= end)
+
+  isBetween1and10 = (target: number) => (target >= 1 && target <= 10)
+
+  isValidScope = (start: number, end: number) => (this.isBetween1and10(start) && this.isBetween1and10(end))
+
   render(start: number, end: number): string{
-    if (start > end) return ''
-    if (!this.isBetween1and10(start) || !this.isBetween1and10(end)) return ''
+    if (!this.isValidRange(start, end)) return ''
+    if (!this.isValidScope(start, end)) return ''
     const expressions = this.buildExpressions(start, end)
     return this.renderExpressions(start, end, expressions)
   }
